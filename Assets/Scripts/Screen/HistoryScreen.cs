d7df0a759595b9838d2c5923610b9ecdc6f4e172
cuongnl <cuongnl@vtdglobal.com>
2024-10-23 13:38:23 +0700

    history

--- Assets/Scripts/Screen/HistoryScreen.cs
+++ Assets/Scripts/Screen/HistoryScreen.cs
@@ -1,9 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class HistoryScreen : MonoBehaviour
 {
+    public GameObject scrollViewContent;
+    public GameObject historyItemPrefab;
+
     public void clickBack() 
     {
         ScreenManager.instance.Pop();
@@ -11,15 +15,44 @@ public class HistoryScreen : MonoBehaviour
 
     void OnEnable()
     {
+        // clear all children
+        foreach (Transform child in scrollViewContent.transform)
+        {
+            Destroy(child.gameObject);
+        }
         // Lấy danh sách lịch sử từ DatabaseManager
         List<History> histories = DatabaseManager.Instance.GetAllHistory();
-        // in
         foreach (History history in histories)
         {
-            Debug.Log("Killed enemies: " + history.killedEnemies);
-            Debug.Log("Survived: " + history.survived);
-            Debug.Log("Level reached: " + history.levelReached);
-            Debug.Log("Timestamp: " + history.timestamp);
+            // Debug.Log("Killed enemies: " + history.killedEnemies);
+            // Debug.Log("Survived: " + history.survived);
+            // Debug.Log("Level reached: " + history.levelReached);
+            // Debug.Log("Timestamp: " + history.timestamp);
+            GameObject historyItem = Instantiate(historyItemPrefab, scrollViewContent.transform);
+            Text killedEnemiesText = getTextComponentByName(historyItem, "KilledEnemiesText");
+            killedEnemiesText.text = history.killedEnemies + "";
+            Text survivedText = getTextComponentByName(historyItem, "SurvivedText");
+            survivedText.text = history.survived;
+            Text levelReachedText = getTextComponentByName(historyItem, "LevelReachedText");
+            levelReachedText.text = history.levelReached + "";
+            Text timestampText = getTextComponentByName(historyItem, "DateTime");
+            timestampText.text = history.timestamp;
+            // set parent
+            historyItem.transform.SetParent(scrollViewContent.transform, false);
         }
+    }
+
+    private Text getTextComponentByName(GameObject parent, string name)
+    {
+        Transform[] allChildren = parent.GetComponentsInChildren<Transform>();
+        foreach (Transform child in allChildren)
+        {
+            Debug.Log(child.name);
+            if (child.name == name)
+            {
+                return child.GetComponent<Text>();
+            }
+        }
+        return null;
     }
 }
